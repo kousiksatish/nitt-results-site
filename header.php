d5702faf4cbd30606fa88fceb185e489dacd508e
Kousik Satish <kousiksatish@gmail.com>
2015-12-26 11:50:54 +0530

    Home page changed

---
+++ header.php
@@ -0,0 +1,63 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="NITT Results">
+<meta name="author" content="Kousik Satish">
+
+<title>NITT Results</title>
+
+<link href="theme/css/bootstrap.min.css" rel="stylesheet">
+
+<link href="theme/css/sb-admin-2.css" rel="stylesheet">
+
+<!-- Custom Fonts -->
+<link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+<link rel="stylesheet" href="theme/css/morris-0.4.3.min.css">
+
+<link href="theme/css/plugins/dataTables/dataTables.bootstrap.css" rel="stylesheet">
+<style>
+	html {
+	  position: relative;
+	  min-height: 50%;
+	}
+	body {
+	  /* Margin bottom by footer height */
+	  margin-bottom: 60px;
+	}
+	.footer {
+	  position: absolute;
+	  bottom: 0;
+	  width: 100%;
+	  /* Set the fixed height of the footer here */
+	  height: 60px;
+	  background-color: #f5f5f5;
+	}
+
+	body > .container {
+	  padding: 60px 15px 0;
+	}
+	.container .text-muted {
+	  margin: 20px 0;
+	}
+
+	.footer > .container {
+	  padding-right: 15px;
+	  padding-left: 15px;
+	}
+
+	code {
+	  font-size: 80%;
+	}
+
+	.footer {
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      height: 80px;
+      background-color:#F8F8F8;
+      color:black;
+      border:1px solid transparent;
+      border-color: #e7e7e7;
+    }
+</style>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<html>
+<!-- <html>
 
 <body>
 <h1>NITT Results</h1>
@@ -22,4 +22,157 @@ Note: Viewable on mobiles
 
 </script>
 </html>
+ -->
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+        include 'header.php';
+    ?>
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">NITT Results</a>
+            </div>
+            <!-- /.navbar-header -->
+            <div class="nav navbar-right">
+              Results are scrapped from nitt.edu, there may be some errors...<br>
+              <a href="javascript:call()"id="issue">Click here</a> to notify such errors
+                  <!-- <a href="#" class="btn btn-primary" id="issues">Any issues? </a> -->
+            </div>
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse collapse">
+                    <ul class="nav" id="side-menu">
+                        <?php
+                            include 'sidebar.php';
+                        ?>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+
+        <div id="page-wrapper">
+            <center>
+                <div id="loader">
+                <br><br><br>
+                <!-- <img src="imgs/ajax-loader.gif"> -->
+                </div>
+            </center>
+            <div id="container">
+                <div class="row">
+                    <center>
+                    <h1 class="page-header">NITT Results </h1>
+                    <h3>With a difference</h3>
+                    <h3><a href="14-18/">Batch 2014 - 2018</a></h3>
+<h3><a href="13-17/">Batch 2013 - 2017</a></h3>
+<h3><a href="12-16/">Batch 2012 - 2016</a></h3>
+<h3><a href="11-15/">Batch 2011 - 2015</a></h3>
+                    </center>
+                </div>
+                <br><br><br>
+                <br><br><br>
+                <br><br><br>
+                
+            </div>
+            <br><br><br>
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+    <footer class="footer">
+        <?php
+            include 'footer.php'
+        ?>
+    </footer>
+    <!-- jQuery -->
+    <script src="../theme/js/jquery-1.11.0.js"></script>
+    <script src="../theme/js/bootstrap.min.js"></script>
+    <script src="../theme/js/morris.js"></script>
+    <script src="../theme/js/raphael-2.1.0.min.js"></script>
+    <script src="../theme/js/plugins/dataTables/jquery.dataTables.js"></script>
+    <script src="../theme/js/plugins/dataTables/dataTables.bootstrap.js"></script>
+    <script>
+
+        $("input[name='dept'],input[name='sem'],input[name='view']").change(function (e){
+            if($(this).is("input[name='dept']"))
+            dept = $(this).val();
+            if($(this).is("input[name='sem']"))
+            sem = $(this).val();
+            if($(this).is("input[name='view']"))
+            url = $(this).val();
+            console.log(dept);
+            $.ajax(
+            {
+                url: url,
+                type: "GET",
+                data: {dept:dept, sem:sem},
+                success:function(data, textStatus, jqXHR)
+                {
+                $('#container').html(data);
+                },
+                error: function(jqXHR, textStatus, errorThrown)
+                {
+
+                }
+
+            });
+        });
+        function call(e){
+            $.ajax(
+            {
+            url: 'issues.php',
+            type: "GET",
+
+            success:function(data, textStatus, jqXHR)
+            {
+            $('#page-wrapper').html(data);
+            },
+            error: function(jqXHR, textStatus, errorThrown)
+            {
+
+            }
+
+            });
+        }
+        $('#loader').hide();
+        $(document).ajaxStart(function(){
+            $('#container').empty();
+            $('#loader').show();
+        });
+
+        $(document).ajaxComplete(function(){
+            $('#loader').hide();
+        });
+
+
+    </script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-71718424-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+
+</html>
